15512ec189a220050c41aad0d76f7642bc3948f3
Arshad3620 <ridoy3620@gmail.com>
2025-07-09 13:48:22 +0900

    feat : add the serializers.py file

--- library Api/django_project/settings.py
+++ library Api/django_project/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-party apps
+    "rest_framework",
+
+    # Local apps
     "books",
+    "apis" #new 
 ]
 
 MIDDLEWARE = [
