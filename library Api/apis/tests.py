260b329af91364f92ce98cf986fd60285a0fd962
Arshad3620 <ridoy3620@gmail.com>
2025-07-09 14:36:48 +0900

    feat:New api updates

--- library Api/apis/tests.py
+++ library Api/apis/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from books.models import Book
 
 # Create your tests here.
+class APITests(APITestCase):
+  @classmethod
+  def setUpTestData(cls):
+    cls.book = Book.objects.create(
+      title = "Django for APIS",
+      subtitle = "Build Web APIS with python and Django ",
+      author = "Arshad",
+      isbn = "1234567890123"
+
+    )
+  def test_api_listview(self):
+        """
+        Ensure we can retrieve the list of books.
+        """
+        response = self.client.get(reverse("book_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Book.objects.count(),1)
+        self.assertContains(response,self.book.title)
