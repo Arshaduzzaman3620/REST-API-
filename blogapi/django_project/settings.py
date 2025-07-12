7b7860a9df4cf05522890d2c09326104538bb0c8
Arshad3620 <ridoy3620@gmail.com>
2025-07-12 22:03:06 +0900

    chore:add the posts app

--- blogapi/django_project/settings.py
+++ blogapi/django_project/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts",
+    "posts",
+
 ]
+AUTH_USER_MODEL = "accounts.CustomUser"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
