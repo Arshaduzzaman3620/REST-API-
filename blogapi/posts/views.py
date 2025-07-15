57bd8bf47fe9bdeec377a1c064e00a6781774a9a
Arshad3620 <ridoy3620@gmail.com>
2025-07-15 20:33:47 +0900

    feat:add the userpermision

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,7 +1,7 @@
 # posts/views.py
 from rest_framework import generics
-
 from .models import Post
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 
 
@@ -13,3 +13,4 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly]
